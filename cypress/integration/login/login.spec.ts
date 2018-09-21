e82b939fa8ba1c6c2692ce3f67a104070e09da7c
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-09-21 14:19:24 +0200

    added cypress

---
+++ cypress/integration/login/login.spec.ts
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+context('Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('.type() - type into a DOM element', () => {
+
+  });
+});
