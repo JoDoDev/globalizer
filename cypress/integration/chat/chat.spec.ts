5c18ceb863747e81f535987e367984771920494e
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-09-24 11:33:39 +0200

    send down old message test

--- cypress/integration/chat/chat.spec.ts
+++ cypress/integration/chat/chat.spec.ts
@@ -1,21 +1,39 @@
 /// <reference types="Cypress" />
 
+function sendMessage(message) {
+  cy.get('[data-cy="message-input"]').type(message);
+  cy.get('[data-cy="submit"]').click();
+}
+
 context('Chat', () => {
   beforeEach(() => {
     cy.clearServer();
-    cy.login('test');
+    cy.login('testUsername');
     cy.visit('/');
   });
 
   it('send message', () => {
     const messageText = "someMessageText";
 
-    cy.get('[data-cy="message-input"]').type(messageText);
-    cy.get('[data-cy="submit"]').click();
+    sendMessage(messageText);
 
     cy.get('[data-cy="chatbox"]').children().as('message');
 
     cy.get('@message').should('have.class', 'own');
+    cy.get('@message').children('p').should('contain', messageText)
+  });
+
+  it('get Previous message', () => {
+    const messageText = "someMessageText";
+
+    sendMessage(messageText);
+
+    cy.login('testUsername2');
+    cy.visit('/');
+
+    cy.get('[data-cy="chatbox"]').children().as('message');
+
+    cy.get('@message').should('not.have.class', 'own');
     cy.get('@message').children('p').should('contain', messageText)
   });
 
